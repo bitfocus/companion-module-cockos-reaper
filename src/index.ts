e356f42d508b3a5b9e26e7144dd3dbed9e3a8d60
Ben Simpson <ben@mrflux.net.nz>
2024-03-16 20:58:59 +1300

    Support for Companion 3.x
    Set up manifest
    
    Typescript setup
    
    Add gitattributes
    
    Beginning porting
    
    Implement actions
    
    Fix track unarm action label
    
    Refactoring module management
    
    Support using variables for custom action
    
    Add refresh OSC action
    
    Adding existing variables (todo FX variables)
    
    Update reaper-osc
    
    Create modulecontext, handle unbinding variables, start implementing feedbacks
    
    More work on feedbacks, implement custom message feedback
    
    Parse variables in value of custom message feedback
    
    Add Track FX variables and more DRY
    
    DRY actions
    
    So DRY
    
    Add missing FX variables
    
    Clear custom feedbacks on destroy
    
    DRY feedbacks
    
    Add more feedbacks
    
    Add Track/FX feedbacks
    
    Add string handler for custom message feedback
    
    Fix Toggle type for Custom OSC action
    
    Remove boolean options from feedback and use built in invert instead
    
    Add .editorconfig
    
    Add presets
    
    Configure .editorconfig to use tabs
    
    Add note about unsupported master track
    
    Fix line endings config in .gitattributes
    
    Fix name of Custom OSC Message feedback

---
+++ src/index.ts
@@ -0,0 +1,204 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import {
+	CompanionVariableValues,
+	InstanceBase,
+	InstanceStatus,
+	runEntrypoint,
+	SomeCompanionConfigField,
+} from '@companion-module/base'
+import { GetConfigFields, ModuleConfig } from './config'
+import { GetPresetsList } from './presets'
+import { CustomMessageFeedbacks, FeedbackBindings, FeedbackId, GetFeedbacksList } from './feedbacks'
+import { GetActionsList } from './actions'
+import { LogLevel, OscMessage, Reaper, ReaperConfiguration } from 'reaper-osc'
+import { GetVariableDefinitions, ReaperProperty } from './variables'
+import { CreateUseInvertForFeedbacksUpgradeScript } from './upgrades'
+
+class ControllerInstance extends InstanceBase<ModuleConfig> {
+	private _reaper: Reaper | null
+	private _unsubscribeVariables: Unsubscribe[] = []
+	private _feedbackBindings: FeedbackBindings = {}
+	private _customMessageFeedbacks: CustomMessageFeedbacks = {}
+
+	constructor(internal: unknown) {
+		super(internal)
+
+		this._reaper = null
+	}
+
+	public async init(config: ModuleConfig): Promise<void> {
+		await this.configUpdated(config)
+
+		this.setActionDefinitions(
+			GetActionsList(() => ({ reaper: this._reaper!, log: (level, message) => this.log(level, message) }))
+		)
+
+		this.setPresetDefinitions(GetPresetsList())
+		this.setFeedbackDefinitions(
+			GetFeedbacksList(() => ({
+				reaper: this._reaper!,
+				log: (level, message) => this.log(level, message),
+				checkFeedback: (feedbackId) => this.checkFeedbacksById(feedbackId),
+				bindings: this._feedbackBindings,
+				customMessageFeedbacks: this._customMessageFeedbacks,
+			}))
+		)
+	}
+
+	public async configUpdated(config: ModuleConfig): Promise<void> {
+		if (this._reaper !== null) {
+			await this.disconnectOsc()
+			this.unbindVariables()
+			this.unbindFeedbacks()
+		}
+
+		const reaperConfig = new ReaperConfiguration()
+
+		// Listener
+		reaperConfig.localAddress = '0.0.0.0'
+		reaperConfig.localPort = config.feedbackPort
+
+		// Reaper
+		reaperConfig.remoteAddress = config.host
+		reaperConfig.remotePort = config.port
+
+		reaperConfig.afterMessageReceived = (message, handled) => {
+			this.handleCustomMessages(message, handled)
+		}
+
+		this._reaper = new Reaper(reaperConfig)
+
+		this.bindVariables()
+
+		this.log('debug', `Reaper Configuration: ${JSON.stringify(reaperConfig, null, 2)}`)
+
+		await this.connectOsc()
+
+		if (config.refreshOnInit) {
+			this.log('info', 'Refreshing OSC values')
+			this._reaper.refreshControlSurfaces()
+		}
+	}
+
+	public getConfigFields(): SomeCompanionConfigField[] {
+		return GetConfigFields()
+	}
+
+	public async destroy(): Promise<void> {
+		await this.disconnectOsc()
+		this.unbindVariables()
+		this.unbindFeedbacks()
+		this.clearCustomFeedbacks()
+	}
+
+	private handleCustomMessages(message: OscMessage, _: boolean) {
+		Object.values(this._customMessageFeedbacks).forEach((feedback) => {
+			feedback.handler.handle(message)
+		})
+	}
+
+	private async connectOsc() {
+		if (this._reaper === null) {
+			this.log('error', 'Reaper is not configured')
+			return
+		}
+
+		this.updateStatus(InstanceStatus.Connecting)
+
+		await this._reaper.start()
+
+		this.updateStatus(InstanceStatus.Ok)
+	}
+
+	private async disconnectOsc() {
+		if (this._reaper === null) {
+			return
+		}
+
+		await this._reaper.stop()
+		this.updateStatus(InstanceStatus.Disconnected)
+		this.log('info', 'Stopped listening to Reaper')
+	}
+
+	private clearCustomFeedbacks(): void {
+		this._customMessageFeedbacks = {}
+	}
+
+	private unbindFeedbacks(): void {
+		const bindings = this._feedbackBindings
+
+		this._feedbackBindings = {}
+
+		for (const entry of Object.entries(bindings)) {
+			entry[1]()
+		}
+	}
+
+	private bindVariables(): void {
+		const variables = GetVariableDefinitions()
+
+		const unsubscribes: Unsubscribe[] = []
+
+		this.setVariableDefinitions(variables)
+
+		const variableValues: CompanionVariableValues = {}
+
+		variables.forEach((variable) => {
+			const property = variable.getProperty(this._reaper!)
+
+			// Bind the variable to the property
+			const unsub = property.item.onPropertyChanged(property.property, () => {
+				this.setVariableValues({
+					[variable.variableId]: this.getCurrentVariableValue(property, variable.valueConverter),
+				})
+			})
+
+			unsubscribes.push(unsub)
+
+			// Set initial value
+			variableValues[variable.variableId] = this.getCurrentVariableValue(property, variable.valueConverter)
+		})
+
+		this.setVariableValues(variableValues)
+
+		this._unsubscribeVariables = unsubscribes
+	}
+
+	private unbindVariables(): void {
+		const unsubscribes = this._unsubscribeVariables
+		this._unsubscribeVariables = []
+
+		unsubscribes.forEach((element) => {
+			element()
+		})
+	}
+
+	private getCurrentVariableValue(property: ReaperProperty, valueConverter?: (value: any) => any) {
+		let value = property.valueSelector(property.item)
+
+		if (valueConverter) {
+			value = valueConverter(value)
+		}
+
+		return value
+	}
+}
+
+export interface ModuleContext {
+	readonly reaper: Reaper
+	readonly log: (level: LogLevel, message: string) => void
+}
+
+type Unsubscribe = () => void
+
+runEntrypoint(ControllerInstance, [
+	CreateUseInvertForFeedbacksUpgradeScript({
+		[FeedbackId.PlayStatus]: { optionId: 'playPause', trueValue: 'Playing' },
+		[FeedbackId.StopStatus]: { optionId: 'stopPlay', trueValue: 'Stopped' },
+		[FeedbackId.RecordStatus]: { optionId: 'recordOrNot', trueValue: 'Recording' },
+		[FeedbackId.RewindStatus]: { optionId: 'rewindOrNot', trueValue: 'Rewinding' },
+		[FeedbackId.ForwardStatus]: { optionId: 'forwardOrNot', trueValue: 'Forwarding' },
+		[FeedbackId.RepeatStatus]: { optionId: 'repeatOrNot', trueValue: 'Active' },
+		[FeedbackId.ClickStatus]: { optionId: 'clickOrNot', trueValue: 'Active' },
+	}),
+])
